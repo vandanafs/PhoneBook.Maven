6b2108d8db88df25b76b58b687b2c7629c70fd09
vandanafs <vandana.f.09@gmail.com>
2022-06-09 14:06:40 -0400

    added files

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -15,40 +12,56 @@ public class PhoneBook {
 
     private final Map<String, List<String>> phonebook;
 
+    //constructor  with Map parameter
     public PhoneBook(Map<String, List<String>> map) {
         this.phonebook = null;
     }
 
-    public PhoneBook() {
-        this(null);
+    //default const
+    public PhoneBook() {this(null);
     }
+
 
     public void add(String name, String phoneNumber) {
+        HashMap<String, String> map=new HashMap<>();
+        map.put(name,phoneNumber);
     }
 
+//adds many phone nums to single name
     public void addAll(String name, String... phoneNumbers) {
-    }
+       // phonebook=new HashMap<String,List<String>>();
+        List<String> phoneNums=new ArrayList<>();
 
+
+    }
+//removes an entry to the composite associate data type
     public void remove(String name) {
     }
 
+    //removes an entry to the composite associate data type
     public Boolean hasEntry(String name) {
         return null;
     }
 
+    //returns a phone number for the respective input name
     public List<String> lookup(String name) {
+
         return null;
     }
 
+    //returns a name for the respective input phoneNumber
     public String reverseLookup(String phoneNumber)  {
         return null;
     }
 
+    //returns a list of all names in this PhoneBook
     public List<String> getAllContactNames() {
+
         return null;
     }
 
     public Map<String, List<String>> getMap() {
+
         return null;
     }
 }
